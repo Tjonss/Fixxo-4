6a711371d6a252376fb12ccf3ebe1413bbec4efd
Michaela <michaela.carlsson@icloud.com>
2022-10-21 13:04:47 +0200

    startsidarosa klar

--- fixxo-frontend/src/App.js
+++ fixxo-frontend/src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
+import FeaturedInstagram from './components/FeaturedInstagram/FeaturedInstagram';
 
 function App() {
   return (
     <div className="App">
-      
+      <FeaturedInstagram />
 
     </div>
   );
