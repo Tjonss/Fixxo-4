bb9e83de41d961ecb9650f1ca094fefd4c8cddc2
Jonas <jonasengstrom_@hotmail.com>
2022-10-29 00:10:43 +0200

    products

--- fixxo-frontend/src/components/FeaturedProducts/SingleFeaturedProduct.jsx
+++ fixxo-frontend/src/components/FeaturedProducts/SingleFeaturedProduct.jsx
@@ -15,16 +15,11 @@ const SingleFeaturedProduct = () => {
 
   return (
     <>
-      <div className="col-6 col-md-4 col-lg-3 col-xl-3">
+      <div className='col-6 col-md-4'>
         <div className="position-relative" 
               onMouseOver={handleMouseOver} 
               onMouseOut={handleMouseOut}>
           <img className='d-block w-100' src="https://images.unsplash.com/photo-1496181133206-80ce9b88a853?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1171&q=80"  />
-        <div>
-          {isHovering && ( 
-            <FeaturedHover />
-          )}
-        </div>
         </div>
         <FeaturedProductsContent/>
       </div>
---
+++ fixxo-frontend/src/views/ProductsView.jsx
@@ -0,0 +1,93 @@
+import React from 'react'
+import SingleFeaturedProduct from '../components/FeaturedProducts/SingleFeaturedProduct'
+
+const ProductsView = () => {
+  return (
+    <>
+      <div className="container mt-5 ">
+        <div className="col-12 container mb-4 d-flex align-items-center">
+          <i className="fa-solid fa-house d-inline fs-"></i>
+          <small className='ms-3'>Categories</small>
+          <i className="fa-solid fa-chevron-right mx-3 small"></i>
+          <small className=''>Product page</small>
+        </div>
+        <div className="container">
+          <div className="row">
+            <div className="All-categories col-3 d-none d-md-block">
+                <p>All Categories</p>
+                <hr />
+                <p className='small mb-2'>Accessories</p>
+                <p className='small mb-2'>Watch Fashion</p>
+                <p className='small mb-2'>Tees, Knits & Polos</p>
+                <p className='small mb-3'>Pants & Denim</p>
+              <div className="Price mb-4">
+                <p>Price</p>
+                <hr />
+                <label htmlFor="customRange2" className="form-label"></label>
+                <input type="range" className="form-range" min="0" max="1000" id="customRange2"></input>
+                <p className='text-end small'>0 - 1000</p>
+              </div>
+              <div className="Size">
+                <p>Sizes</p>
+                <hr />
+                <p className='small mb-2'>Small</p>
+                <p className='small mb-2'>Medium</p>
+                <p className='small mb-2'>Large</p>
+                <p className='small mb-3'>Extra Large</p>
+              </div>
+              <div className="Color">
+                <p>Color</p>
+                <hr />
+                <div className="Products-Colors mb-3">
+                  <div>
+                    <input type="checkbox" name="black" id="forblack" />
+                    <label htmlFor='black' className='small mb-2 ms-2'>Black</label>
+                  </div>
+                  <div>
+                    <input type="checkbox" name="black" id="forblue" />
+                    <label htmlFor='blue' className='small mb-2 ms-2'>Blue</label>
+                  </div>
+                  <div>
+                    <input type="checkbox" name="black" id="forred" />
+                    <label htmlFor='red' className='small mb-2 ms-2'>Red</label>
+                  </div>
+                  <div>
+                    <input type="checkbox" name="black" id="forgreen" />
+                    <label htmlFor='green' className='small mb-2 ms-2'>Green</label>
+                  </div>
+                </div>
+                <div className="Brands">
+                  <p>Brands</p>
+                  <hr />
+                  <p className='small mb-2'>Accessories</p>
+                  <p className='small mb-2'>Watch Fashion</p>
+                  <p className='small mb-2'>Tees, Knits & Polos</p>
+                  <p className='small mb-3'>Pants & Denim</p>
+                </div>
+              </div>
+          </div>
+          <div className="col-12 col-md-9">
+            <div className="row text-center gy-2">
+              <SingleFeaturedProduct/>
+              <SingleFeaturedProduct/>
+              <SingleFeaturedProduct/>
+              <SingleFeaturedProduct/>
+              <SingleFeaturedProduct/>
+              <SingleFeaturedProduct/>
+              <SingleFeaturedProduct/>
+              <SingleFeaturedProduct/>
+              <SingleFeaturedProduct/>
+              <SingleFeaturedProduct/>
+              <SingleFeaturedProduct/>
+              <SingleFeaturedProduct/>
+            </div>
+          </div>
+          </div>
+        </div>
+      </div>
+    </>
+  )
+}
+
+
+export default ProductsView
